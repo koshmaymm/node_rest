5d2a55d50f1df6b9928847837824894a88ed835e
koshmaymm <koshmaymm@ukr.net>
2018-11-12 18:44:14 +0200

    added json file

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ var fs = require('fs');
 
 var app = express();
 
-var path = 'logger.txt';
+var path = 'data.json';
 
 app.use(function(request, response, next){
-    var data = `Address : ${request.ip}; Time: ${new Date().toLocaleString()}; URL : ${request.url}\n`;
+   // var data = `Address : ${request.ip}; Time: ${new Date().toLocaleString()}; URL : ${request.url}\n`;
+   var data = JSON.stringify({
+    "Time" :`${new Date().toLocaleString()}`
+   });
+  
 
     fs.appendFile(path, data, function(err){
         console.log('data wrote');
