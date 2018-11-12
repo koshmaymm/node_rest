3620684f2f7dcefd3a4d433d8b2415fced448441
koshmaymm <koshmaymm@ukr.net>
2018-11-12 12:26:26 +0200

    added append file

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+
 var express = require('express');
+var fs = require('fs');
+
 var app = express();
 
+var path = 'logger.txt';
+
+app.use(function(request, response, next){
+    var data = `Address : ${request.ip}; Time: ${new Date().toLocaleString()}; URL : ${request.url}\n`;
+
+    fs.appendFile(path, data, function(err){
+        console.log('data wrote');
+        next();
+    });
+});
+
+app.get('/', function(request, response){
+    console.log(request.url);
+    response.send('<h1>Hello, world!</h1>');
+});
+
 app.listen(8080, function(){
-    console.log('server start on port 8080');
-})
+    console.log('Server start')
+});
+
+
+
+
