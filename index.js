7c26b67566382815fe1b367a112925554059c32e
koshmaymm <koshmaymm@ukr.net>
2018-11-12 21:32:48 +0200

    changed dependencies mongodb

--- index.js
+++ index.js
@@ -7,14 +7,35 @@ var app = express();
 var path = 'data.json';
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = "mongodb://localhost:27017/drinksCollectionDB";
 
 
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    console.log("Database created!");
-    db.close();
-  });
+var users = [
+    {name: "Absinthe", price: 73}, 
+    {name: "Beer", price: 5}, 
+    {name: "Brandy", price: 45},
+    {name: "Whisky", price: 34},
+    {name: "Wine", price: 50}
+];
+ 
+MongoClient.connect(url, function(err, db){
+     
+    var collection = db.collection("users");
+    collection.insertMany(users, function(err, results){
+             
+        collection.findOneAndUpdate(
+            {price: 25}, 
+            { $set: {price: 21}},    
+            {                            
+                 returnOriginal: false
+            },
+            function(err, result){                 
+                console.log(result);
+                db.close();
+            }
+        );
+    });
+});
 
 app.use(function(request, response, next){
    var data = JSON.stringify({
