3a05d69b133ae2724b43e38cc63220be15d88426
koshmaymm <koshmaymm@ukr.net>
2018-11-12 21:04:48 +0200

    added package.json and npm installed

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ var app = express();
 
 var path = 'data.json';
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+  });
+
 app.use(function(request, response, next){
    var data = JSON.stringify({
     "Time" :`${new Date().toLocaleString()}`
