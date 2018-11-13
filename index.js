66515dd22075f18cb79663b73b9d2b7f0bb7cbc4
koshmaymm <koshmaymm@ukr.net>
2018-11-13 15:39:27 +0200

    added writting file

--- index.js
+++ index.js
@@ -9,40 +9,34 @@ var drinks = require('./data');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/drinksCollectionDB";
 
-var drinkOne = [
-    {name: "WineOld", price: 68}
-];
+var drinkOne = {name: "WineOld", price: 68};
 
 MongoClient.connect(url, function(err, db){
     var collection = db.collection("drinks");
 
     collection.insertMany(drinks, function(err, results){
-        if(err) throw err;
-
-        console.log(results);       
+        if(err) throw err;       
         db.close();
     });    
 });
 
-/*app.use(function(request, response, next){
-   var data = JSON.stringify({
-    "Time" :`${new Date().toLocaleString()}`
-   });
-   
-    fs.writeFile(path, data, function (err) {
+app.use(function(request, response, next){
+    var newBar = drinks;
+        newBar.push(drinkOne);
+
+    fs.writeFile("./data.json", JSON.stringify(newBar), function (err) {
         if (err) throw err;
         console.log('Replaced!');
         next();
     });
-});*/
+});
 
 app.get('/', function(request, response){
-    console.log(request.url);
-    response.send('<h1>Hello, world!</h1>');
+    response.send('<h1>Hello, Bar!</h1>');
 });
 
 app.listen(8080, function(){
-    console.log('Server start')
+    // console.log('Server start')
 });
 
 
