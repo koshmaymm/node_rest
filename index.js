062943a233e5fe8783e0422c55b29063519b3d8d
koshmaymm <koshmaymm@ukr.net>
2018-11-13 14:50:08 +0200

    fixed name

--- index.js
+++ index.js
@@ -4,51 +4,22 @@ var fs = require('fs');
 
 var app = express();
 
-var persons = require('./data');
+var drinks = require('./data');
 
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/drinksCollectionDB";
 
-
-var drinks = [
-    {name: "Absinthe", price: 73}, 
-    {name: "Beer", price: 5}, 
-    {name: "Brandy", price: 45},
-    {name: "Whisky", price: 34},
-    {name: "Wine", price: 50}
-];
-
 var drinkOne = [
     {name: "WineOld", price: 68}
 ];
- 
-/*MongoClient.connect(url, function(err, db){
-    var collection = db.collection("drinks");
-    collection.insertMany(drinks, function(err, results){     
-        collection.findOneAndUpdate(
-            {price: 25}, 
-            { $set: {price: 21}},    
-            {                            
-                 returnOriginal: false
-            },
-            function(err, result){                 
-                console.log(result);
-                db.close();
-            }
-        );
-    });
-});*/
 
 MongoClient.connect(url, function(err, db){
     var collection = db.collection("drinks");
-    // метод insertMany используется для добавления множества обьектов
-    collection.insertMany(drinkOne, function(err, results){
+
+    collection.insertMany(drinks, function(err, results){
         if(err) throw err;
 
-        console.log('Data added!');
-        console.log('********** Result **********');
-        console.log(results);
-        console.log('****************************');        
+        console.log(results);       
         db.close();
     });    
 });
