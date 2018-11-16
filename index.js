0fb3fc93a1f653c4a0d45861672b8409f2a4276b
koshmaymm <koshmaymm@ukr.net>
2018-11-16 18:04:44 +0200

    added async await

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const url = "mongodb://localhost:27017/drinksCollectionDB";
 const newPrice = ~~(Math.random()*100);
 const drinkOne = {name: "WineOld", price: newPrice};
 
-const sendPostRequest = (request, response) => {
-    if(request && response) {
+ const sendPostRequest = async (request, response) => {
+    try {
         MongoClient.connect(url, function(err, db){
             const drinks_collection = db.collection('drinks_in_bar');
                 drinks_collection.insertOne(drinkOne, function(err, result){
@@ -19,10 +19,12 @@ const sendPostRequest = (request, response) => {
                     db.close();
                 });
         }); 
+        response.json(drinkOne);
+    } catch (err) {
+      console.log(err);
+      next(); 
     }
-    response.send('<h2>New drink was added !!!</h2>');
-    // res.json();
-}
+} 
 
 app.get('/', function(request, response){
     response.send('<h1>Hello, Bar!</h1>');
@@ -33,23 +35,3 @@ app.post('/', sendPostRequest);
 app.listen(8080, function(){
     // console.log(drinkOne)
 });
-
-/*
-function(request, response){
-
-    if(request && response) {
-        MongoClient.connect(url, function(err, db){
-            const drinks_collection = db.collection('drinks_in_bar');
-                drinks_collection.insertOne(drinkOne, function(err, result){
-                    if(err){
-                        console.log(err);
-                        return;
-                    }
-
-                    db.close();
-                });
-        }); 
-    }
-    response.send('<h2>New drink was added !!!</h2>');
-  }
-  */
