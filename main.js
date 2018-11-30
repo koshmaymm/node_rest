f405db455f15edb5b4e52b159f26a260d00ae979
koshmaymm <koshmaymm@ukr.net>
2018-11-30 16:21:54 +0200

    added categories to query

--- main.js
+++ main.js
@@ -7,15 +7,32 @@ const url = "mongodb://localhost:27017/mydb";
 
 const getValue = async (err, db) => {
         if (err) throw err;
-        var query = { some_id: 3 };
-        await db.collection("contributors").find(query).toArray(function(err, result) {
+        const query = { 
+            some_id: { $gt: 640 } 
+        }
+        // { categories: 1, pageCount: 1 }
+        // some_id: 3,
+        // some_id: { $lt: 3}
+        // categories: ['Java'],
+        // categories: ['Open Source', 'Mobile']
+        // pageCount: { $gt: 400 } // more then 400
+        //{ pageCount: { $gt: 600 } }
+
+        const resCategories = { 
+            categories: 1, 
+            pageCount: 1,
+            // publishedDate: 1,
+            status: 1
+        }
+
+        await db.collection("contributors").find(query,resCategories).toArray(function(err, result) {
             if (err) throw err;
             console.log(result);
             db.close();
-        }); 
+        });
 }
 
-const sendRequest = async (request, response, db) => {
+const sendRequest = (request, response, db) => {
     try {
         MongoClient.connect(url, getValue);
         response.json();
