cc478793a6a55e091ac35f2941eca01f7d5d32f5
koshmaymm <koshmaymm@ukr.net>
2018-12-03 21:07:26 +0200

    added app_books

---
+++ app_books.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+app.use(bodyParser.json());
+
+Book = require('./models/book');
+
+mongoose.connect('mongodb://localhost:27017/bookstore');
+const db = mongoose.connection;
+
+app.get('/', (req, res) => {
+	res.send('Please use api books');
+});
+
+app.get('/api/books', (req, res) => {
+	Book.getBooks((err, books) => {
+		if(err){
+			throw err;
+		}
+		res.json(books);
+	});
+});
+
+app.get('/api/books/:_id', (req, res) => {
+	Book.getBookById(req.params._id, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.post('/api/books', (req, res) => {
+	const book = req.body;
+	Book.addBook(book, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.put('/api/books/:_id', (req, res) => {
+	const id = req.params._id;
+    const book = req.body;
+
+	Book.updateBook(id, book, {}, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.delete('/api/books/:_id', (req, res) => {
+	const id = req.params._id;
+	Book.removeBook(id, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.listen(3000);
+console.log('Running on port 3000...');
---
+++ models/book.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+
+const bookSchema = mongoose.Schema({
+	title:{
+		type: String,
+		required: true
+    },
+    pageCount: {
+        type: String,
+        required: true
+    },
+    publishedDate:{
+		type: String,
+		default: Date.now
+    },
+    thumbnailUrl: {
+        type: String,
+		required: true
+    },
+    status: {
+        type: String,
+		default: "UNPUBLISHED"
+    },
+	authors:{
+		type: Array,
+		required: true
+    },
+    categories: {
+        type: Array,
+		required: true 
+    }
+});
+
+const Book = module.exports = mongoose.model('Book', bookSchema);
+
+module.exports.getBooks = (callback, limit) => {
+	Book.find(callback).limit(limit);
+}
+
+module.exports.getBookById = (id, callback) => {
+	Book.findById(id, callback);
+}
+
+module.exports.addBook = (book, callback) => {
+	Book.create(book, callback);
+}
+
+module.exports.updateBook = (id, book, options, callback) => {
+    const query = {_id: id };
+	const update = {
+		title: book.title,
+		pageCount: book.pageCount,
+        publishedDate: book.publishedDate,
+        thumbnailUrl: book.thumbnailUrl,
+        status: book.status,
+        authors: [book.authors],
+        categories: [book.categories],
+	}
+	Book.findOneAndUpdate(query, update, options, callback);
+}
+
+module.exports.removeBook = (id, callback) => {
+	const query = {_id: id};
+	Book.remove(query, callback);
+}
