61ee2be8173fd632ca79752b951d1cf1f371dd8e
koshmaymm <koshmaymm@ukr.net>
2018-12-03 21:06:37 +0200

    fixed data for database

---
+++ some_data/books_data.js
@@ -0,0 +1,128 @@
+const books = [
+    {
+      'title': 'Unlocking Android',
+      'pageCount': 416,
+      'publishedDate': '2009-04-01T00:00:00.000-0700' ,
+      'thumbnailUrl': 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson.jpg',
+      'status': 'PUBLISH',
+      'authors': ['W. Frank Ableson', 'Charlie Collins', 'Robi Sen'],
+      'categories': ['Open Source', 'Mobile']
+    },
+    {
+      'title': 'Android in Action, Second Edition',
+      'pageCount': 592,
+      'publishedDate': '2011-01-14T00:00:00.000-0800' ,
+      'thumbnailUrl': 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson2.jpg',
+      'status': 'PUBLISH',
+      'authors': ['W. Frank Ableson', 'Robi Sen'],
+      'categories': ['Java']
+    },
+    {
+      'title': 'Specification by Example',
+      'pageCount': 0,
+      'publishedDate': '2011-06-03T00:00:00.000-0700',
+      'thumbnailUrl': 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/adzic.jpg',
+      'status': 'PUBLISH',
+      'authors': ['Gojko Adzic'],
+      'categories': ['Software Engineering']
+    },
+    {
+      'title': 'Flex 3 in Action',
+      'pageCount': 576,
+      'publishedDate': '2009-02-02T00:00:00.000-0800',
+      'thumbnailUrl': 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ahmed.jpg',
+      'status': 'PUBLISH',
+      'authors': ['Tariq Ahmed with Jon Hirschi', 'Faisal Abid'],
+      'categories': ['Internet']
+    },
+    {
+      'title': 'Flex 4 in Action',
+      'pageCount': 600,
+      'publishedDate': '2010-11-15T00:00:00.000-0800',
+      'thumbnailUrl': 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ahmed2.jpg',
+      'status': 'PUBLISH',
+      'authors': ['Tariq Ahmed', 'Dan Orlando', 'John C. Bland II', 'Joel Hooks'],
+      'categories': ['Internet']
+    },
+    {
+      'title': 'Collective Intelligence in Action',
+      'pageCount': 425,
+      'publishedDate': '2008-10-01T00:00:00.000-0700',
+      'thumbnailUrl': 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/alag.jpg',
+      'status': 'PUBLISH',
+      'authors': ['Satnam Alag'],
+      'categories': ['Internet']
+    },
+    {
+      'title': 'Zend Framework in Action',
+      'pageCount': 432,
+      'publishedDate': '2008-12-01T00:00:00.000-0800' ,
+      'thumbnailUrl': 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/allen.jpg',
+      'status': 'PUBLISH',
+      'authors': ['Rob Allen', 'Nick Lo', 'Steven Brown'],
+      'categories': ['Web Development']
+    },
+    {
+      'title': 'Flex on Java',
+      'pageCount': 265,
+      'publishedDate': '2010-10-15T00:00:00.000-0700',
+      'thumbnailUrl': 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/allmon.jpg',
+      'status': 'PUBLISH',
+      'authors': ['Bernerd Allmon', 'Jeremy Anderson'],
+      'categories': ['Internet']
+    },
+    {
+      'title': 'Griffon in Action',
+      'pageCount': 375,
+      'publishedDate': '2012-06-04T00:00:00.000-0700',
+      'thumbnailUrl': 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/almiray.jpg',
+      'status': 'PUBLISH',
+      'authors': ['Andres Almiray', 'Danno Ferrin', '', 'James Shingler'],
+      'categories': ['Java']
+    },
+    {
+      'title': 'OSGi in Depth',
+      'pageCount': 325,
+      'publishedDate': '2011-12-12T00:00:00.000-0800',
+      'thumbnailUrl': 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/alves.jpg',
+      'status': 'PUBLISH',
+      'authors': ['Alexandre de Castro Alves'],
+      'categories': ['Java']
+    },
+    {
+      'title': 'Flexible Rails',
+      'pageCount': 592,
+      'publishedDate': '2008-01-01T00:00:00.000-0800' ,
+      'thumbnailUrl': 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/armstrong.jpg',
+      'status': 'PUBLISH',
+      'authors': ['Peter Armstrong'],
+      'categories': ['Web Development']
+    },
+    {
+      'title': 'Hello! Flex 4',
+      'pageCount': 258,
+      'publishedDate': '2009-11-01T00:00:00.000-0700' ,
+      'thumbnailUrl': 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/armstrong3.jpg',
+      'status': 'PUBLISH',
+      'authors': ['Peter Armstrong'],
+      'categories': ['Internet']
+    },
+    {
+      'title': 'Coffeehouse',
+      'pageCount': 316,
+      'publishedDate': '1997-07-01T00:00:00.000-0700',
+      'thumbnailUrl': 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/asher.jpg',
+      'status': 'PUBLISH',
+      'authors': ['Levi Asher', 'Christian Crumlish'],
+      'categories': ['Miscellaneous']
+    },
+    {
+      'title': 'Team Foundation Server 2008 in Action',
+      'pageCount': 344,
+      'publishedDate': '2008-12-01T00:00:00.000-0800',
+      'thumbnailUrl': 'https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/azher.jpg',
+      'status': 'PUBLISH',
+      'authors': ['Jamil Azher'],
+      'categories': ['Microsoft .NET']
+    }
+]
